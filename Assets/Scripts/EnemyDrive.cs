0a5556cf76d1a5eea512b23611cc584a966f07d6
Chris DeLeon <gamkedo@gmail.com>
2017-04-01 22:48:29 -0700

    Enemy vehicles now drive automatically (currently random) in field, refactored player hovercraft code to be shared with the enemy in a superclass

---
+++ Assets/Scripts/EnemyDrive.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDrive : HoverCraftBase {
+	protected override void Init () {
+		StartCoroutine(ChangeDir());
+	}
+
+	IEnumerator ChangeDir() {
+		while(true) {
+			turnControl = Random.Range(0.0f, 1.0f) - Random.Range(0.0f, 1.0f);
+			gasControl = Random.Range(0.6f, 0.9f);
+			yield return new WaitForSeconds( Random.Range(0.5f, 3.0f) );
+		}
+	}
+
+	protected override  void Tick() {
+		
+	}
+}
