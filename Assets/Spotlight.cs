759b43e4aed11a5ef4a51c351ee19cdd56f4b7c0
HARDLINE\Anthony <lordrahvin@gmail.com>
2017-05-10 21:06:04 -0700

    Expanded the size of the Bowl
    Removed AI alert colliders, removed collider walls, expanded scenary
    obejcts to 2x scale, made the spotlights rotate

---
+++ Assets/Spotlight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spotlight : MonoBehaviour {
+
+	Vector3 centralVector;
+	float dir = 1f;
+	float rotationSpeed = 5f;
+	
+	private void Start() 
+	{
+		Vector3 primaryFocusPoint = transform.position + transform.forward;
+		centralVector = primaryFocusPoint - transform.position;
+	}
+
+	private void Update()
+	{
+		if (Vector3.Angle(transform.forward, centralVector) > 15f)
+		{
+			dir = dir * -1f;		
+		}
+		transform.Rotate(0,dir * Time.deltaTime * rotationSpeed, 0);
+
+	}
+
+
+
+}
