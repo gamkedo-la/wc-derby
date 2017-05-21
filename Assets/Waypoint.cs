2a28433d928de4ad12bcf4a181750eb94b9b4a46
Chris DeLeon <gamkedo@gmail.com>
2017-05-21 02:16:33 -0700

    Debug/editor display added for waypoint data updated to support variable track thickness, consider waypoint orientation, test data defined for camber map. Not yet used by AI

---
+++ Assets/Waypoint.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Waypoint : MonoBehaviour {
+	public Transform[] next;
+
+	void OnDrawGizmosSelected() {
+	}
+
+	[DrawGizmo(GizmoType.NonSelected | GizmoType.Selected)]
+	static void DrawGizmoForMyScript(Waypoint obj, GizmoType gizmoType)
+	{
+		Vector3 position = obj.transform.position;
+
+		Vector3 barDim = Vector3.one * 40.0f;
+		Gizmos.color = Color.red;
+		Gizmos.DrawCube(position, barDim);
+		barDim = Vector3.one * 40.0f;
+
+		if(obj.next != null && obj.next.Length > 0) {
+			Gizmos.color = Color.green;
+			for(int i = 0; i < obj.next.Length; i++) {
+				Gizmos.DrawLine(position, obj.next[i].position);
+				//Vector3 barDim = Vector3.one * 40.0f;
+				//Gizmos.DrawCube(transform.position, barDim);
+			}
+		}
+
+		float trackWidthHere = obj.transform.localScale.x;
+		Vector3 trackPerpLine = obj.transform.right;
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawLine(position, position+0.5f*trackPerpLine*trackWidthHere);
+		Gizmos.DrawLine(position, position-0.5f*trackPerpLine*trackWidthHere);
+		for(int i=0;i<obj.next.Length;i++) {
+			Vector3 nextPos = obj.next[i].position;
+			float nextWidthHere = obj.next[i].localScale.x;
+			Vector3 nextPerpLine = obj.next[i].right;
+
+			Gizmos.color = Color.cyan;
+			Gizmos.DrawLine(nextPos+0.5f*nextPerpLine*nextWidthHere, position+0.5f*trackPerpLine*trackWidthHere);
+			Gizmos.DrawLine(nextPos-0.5f*nextPerpLine*nextWidthHere, position-0.5f*trackPerpLine*trackWidthHere);
+		}
+
+	}
+}
